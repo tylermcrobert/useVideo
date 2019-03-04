5e33806076266abfd06c35e09c8c78b594e2756d
Tyler <tyler.mcrobert@gmail.com>
2019-03-03 19:22:15 -0800

    Fixed working example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # usevideo
 
-> 
+> useVideo hook 
 
 [![NPM](https://img.shields.io/npm/v/usevideo.svg)](https://www.npmjs.com/package/usevideo) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import useVideo from 'usevideo'
+import './styles.css'
 
-import './index.css'
-import App from './App'
+function App() {
+  const { ref, state, functions } = useVideo(
+    <video muted autoPlay loop style={{ width: '100%' }}>
+      <source
+        src='https://app.coverr.co/s3/mp4/4X4-in-Vinyard.mp4'
+        type='video/mp4'
+      />
+    </video>
+  )
+  return (
+    <div className='App'>
+      <video ref={ref} muted autoPlay loop style={{ width: '100%' }}>
+        <source
+          src='https://app.coverr.co/s3/mp4/4X4-in-Vinyard.mp4'
+          type='video/mp4'
+        />
+      </video>
+      {state.ready && (
+        <>
+          <p>ready: {state.ready.toString()}</p>
+          <p>playing: {state.isPlaying.toString()}</p>
+          <p>duration: {state.duration}s</p>
+          <p>muted: {state.muted.toString()}</p>
+          <p>time: {state.time}s</p>
+          <p>percent: {state.percent}%</p>
+          {state.isPlaying ? (
+            <button onClick={functions.pause}>Pause</button>
+          ) : (
+            <button onClick={functions.play}>play</button>
+          )}
+          {state.muted ? (
+            <button onClick={functions.unmute}>Unmute</button>
+          ) : (
+            <button onClick={functions.mute}>Mute</button>
+          )}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+          <input
+            type='range'
+            min='0'
+            max={state.duration}
+            value={state.time}
+            onChange={functions.seek}
+          />
+        </>
+      )}
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
